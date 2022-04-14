e93e2361c498637041a20988c83a1ef705b85a7d
malkawenedal <malkawenedal@gmail.com>
2022-04-13 19:08:31 -0600

    add the fallback function

--- Starter_Code/joint_savings.sol
+++ Starter_Code/joint_savings.sol
@@ -90,5 +90,5 @@ contract JointSavings {
     /*
     Finally, add the **default fallback function** so that your contract can store Ether sent from outside the deposit function.
     */
-    // YOUR CODE HERE!
+   function() external payable{}
 }

3b69fad79d773144922f3914e270aad221bb58e3
malkawenedal <malkawenedal@gmail.com>
2022-04-13 19:05:26 -0600

    Call the  variable and set it equal to the balance of the contract

--- Starter_Code/joint_savings.sol
+++ Starter_Code/joint_savings.sol
@@ -74,7 +74,7 @@ contract JointSavings {
         /*
         Call the `contractBalance` variable and set it equal to the balance of the contract by using `address(this).balance`.
         */
-        // YOUR CODE HERE!
+        contractBalance=address(this).balance;
     }
 
     /*
